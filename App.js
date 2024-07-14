7269c8b074fb074431f2547c14a8ba6e6f594134
AJN <buzz781@gmail.com>
2024-07-14 20:37:41 +0900

    <Study3> 클래스형, 함수형 컴포넌트 생명주기 실습 및 useEffect 학습

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image,Button } from "react-native";
 import StateWithClassComponent from './StateWithClassComponent';
 import StateWithFuctionalComponent from './StateWithFuctionalComponent';
+import UseEffectWithClassComponent from './UseEffectWithClassComponent'
+import UseEffectWithFunctionalComponent from './UseEffectWithFunctionalComponent'
+import CustomHook from './CustomHook'
 
 export default function App() {
+  const [isTrue,setIsTrue] = useState(true);
   return (
     <View style={styles.container}>
-      {/* <StateWithClassComponent /> */}
-      <StateWithFuctionalComponent />
+      {/* <StateWithClassComponent />
+      <StateWithFuctionalComponent /> */}
+      {/* {isTrue ? <UseEffectWithClassComponent /> : null} */}
+      {/* <UseEffectWithFunctionalComponent />
+      <Button title="toggle" onPress={()=>setIsTrue(!isTrue)}/> 
+       */}
+       
     </View>
   );
 }
---
+++ CustomHook.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+
+const CustomHook = () => {
+  const [name,setName] = useState('');
+  const [age,setAge] = useState('');
+  const [city,setCity]= useState('');
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default CustomHook;
